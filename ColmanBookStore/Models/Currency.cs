5ba25c7922d5791fe476881adb8f308f6b53e420
shayco27 <ShayCohen1027@gmail.com>
2021-07-04 20:45:16 +0300

    added relevant models

---
+++ ColmanBookStore/Models/Currency.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+namespace ColmanBookStore.Models
+{
+    public enum Currency
+    {
+        USD = '$',
+        EUR = '€',
+        ILS = '₪',
+    }
+}
---
+++ ColmanBookStore/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ColmanBookStore.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        public Costumer Costumer { get; set; }
+        public OrderStatus Status { get; set; }
+        public string Address { get; set; }
+        public string Zip { get; set; }
+        public string CCName { get; set; }
+        public string CCNumber { get; set; }
+        public string CCExpiration { get; set; }
+        public string CCCvv { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+    }
+}
